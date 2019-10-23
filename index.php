b5466eb377b0df362051a023253c0f8895104feb
Nurbek Ismoilov <nuronbeck@gmail.com>
2019-10-24 00:49:06 +0500

    ONE COMMIT

---
+++ index.php
@@ -0,0 +1,252 @@
+<?php if(!isset($send_count)){$send_count = 0;} ?>
+
+
+<?php if(isset($_POST['callbackagain']))
+	{
+		unset($_POST['callback']);
+		$send_count = 0;
+		unset($_POST['callbackagain']);
+	} 
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Барбекю, тандыр, печьи и многое для вашего дома! Строительство летней кухни в Узбекистане!</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="Строительство летней кухни в Узбекистане! Тандыр, барбекю, пицца печь, костёр и много другое!">
+		<meta name="keywords" content="тандыр учок барбекю пицца печь летняя кухня Узбекистан строительство tandir o`choq qurish barbekyu pitsa pech uyingizga kerak bo`lgan kamina tandir tayyorlash usta arzon qilish remont Toshkent Andijon Samarqand Xiva Buxoro Namangan butun O`zbekiston летний кухня киламиз камен">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper-->
+			<div id="wrapper">
+
+				<!-- Nav -->
+					<nav id="nav">
+						<a href="#" class="icon fa-home"><span>Главная</span></a>
+						<a href="#work" class="icon fa-folder"><span>Наши работы</span></a>
+						<a href="#contact" class="icon fa-envelope"><span>Обратная связь</span></a>
+						<a href="https://t.me/Rovshanchik" class="icon fa-telegram"><span>Telegram</span></a>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Me -->
+							<article id="home" class="panel intro">
+								<header>
+								<!--
+									<h1>Равшан</h1>
+									<p>Главный мастер</p>
+								-->
+									<h1>Строительство летней кухни в Узбекистане</h1>
+									<p>(тандыр, барбекю, печьи ..)</p>
+
+								</header>
+								<a href="#work" class="jumplink pic">
+									<span class="arrow icon fa-chevron-right"></span>
+									<img src="images/me.jpg" alt="" />
+								</a>
+							</article>
+
+						<!-- Work -->
+							<article id="work" class="panel">
+								<header>
+									<h2>Наши работы</h2>
+								</header>
+								<p>
+									Здравствуйте! Вы попали на эту страницу, потому что интересуетесь изготовлением кирпичных каминов, печей, тандыров, каминопечей и барбекю?<br>
+									Наша бригада уже <b>более 5 лет</b> занимается изготовлением, проектированием, строительством каминов, тандыров и печей в Узбекистане и не только.
+									Наш ресурс дает возможность выбрать интересующий Вас кирпичный камин или печь, а также воспользоваться услугами настоящих профессионалов, которые имеют уникальный опыт в этой области. Можем предложить разнообразные аксессуары и комплектующие от производителей, которые известны в <b>США</b> и <b>Европе</b>.
+									<br>
+									Вы можете сами убедиться в качестве работ, посмотрев наши работы в галерее. Там представлены большинство проектов, которые мы построили на территории <b>Узбекистана</b> и <b>Казахстана</b>.
+								</p>
+								<p>
+								    <div style="position:relative;padding-bottom:56.25%; padding-top:25px;height:0;">
+								        <iframe width="560" height="315" style="position:absolute;top:0;left:0;width:100%;height:100%;" src="https://www.youtube.com/embed/Gm9HyCS9qVw" frameborder="0" allowfullscreen></iframe>
+								    </div>
+								</p>
+								<header>
+									<h2>Галерея</h2>
+								</header>
+								<section>
+									<div class="row">
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic01.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic02.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic03.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic04.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic05.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic06.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic07.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic08.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic09.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic10.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic11.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/pic12.jpg" alt=""></a>
+										</div>
+									</div>
+								</section>
+								
+								<header>
+									<h2>5 причин заказать летнюю кухню и прочее у нас </h2>
+								</header>
+								<p>
+									Мы предлагаем <b>лучшие цены</b> на летние кухни! <br>
+									Мы строим из <b>самого лучшего материала</b>! <br>
+									Профессиональные бригады строителей - работа будет выполнена <b>качественно</b> и <b>в срок</b> !<br>
+									<b>Оптимизированные</b> логистические схемы перевозок !<br>
+									Оптовые цены на все виды строительных и отделочных материалов - ведь сотрудничество с заказчиком должно быть <b>ВЗАИМОВЫГОДНЫМ</b>!
+								</p>
+								<header>
+									<h2>Прайс лист</h2>
+								</header>
+								<section>
+									<div class="row">
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/a1.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/a2.jpg" alt=""></a>
+										</div>
+										<div class="col-4 col-6-medium col-12-small">
+											<a href="#" class="image fit"><img src="images/a3.jpg" alt=""></a>
+										</div>
+									</div>
+								</section>
+								<p>
+									<b>
+									Цена может измениться в зависимости от сложности работы! Все представленные выше цены является стандартными.
+									</b>
+									<br><br> Подробности по телефону 
+									<b>+998 97 720 43 93, +998 99 841 33 43</b>
+								</p>
+							</article>
+
+						<!-- Contact -->
+							<article id="contact" class="panel">
+								<header>
+									<h2>Мы перезвоним Вам</h2>
+								</header>
+	<?php
+		if (!isset($_POST['callback'])) :?>
+						            
+	<?php 
+		$send_count = 0;
+	?>
+								<form action="" method="POST">
+									<div>
+										<div class="row">
+											<div class="col-6 col-12-medium">
+												<input type="text" name="name" placeholder="Имя" />
+											</div>
+											<div class="col-6 col-12-medium">
+												<input type="text" name="lastname" placeholder="Фамилия" />
+											</div>
+											<div class="col-12">
+												<input type="text" name="telephone" placeholder="Номер телефона" />
+											</div>
+											<div class="col-12">
+												<textarea name="message" placeholder="Сообщение" rows="6"></textarea>
+											</div>
+											<div class="col-12">
+												<input type="submit" name="callback" value="Отправить" />
+											</div>
+										</div>
+									</div>
+								</form>
+
+	<?php elseif (isset($_POST['callback'])) : ?>
+		<?php
+	        if($send_count == 0)
+	        {
+				$name = $_POST['name'];
+				$lastname = $_POST['lastname'];
+				$telephone = $_POST['telephone'];
+				$message = $_POST['message'];
+
+
+	            $token = '641058814:AAE8gCbwPTWFhnmuKvGHTP5Uh_exC7gBJwA';
+	            $id = '325788921';  // Это твой id в телеграмме
+	            $reply = "❗️Обратный звонок!\n"."\n"."👤 Посетитель: ".$name." ".$lastname."\n"."☎️ Тел. : ".$telephone."\n"."✉️ Сообщение: ".$message;
+	            $params = array(
+	                "chat_id" => $id,
+	                "text" => $reply
+	            );
+	            
+	            $request_url = 'https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($params);
+	            file_get_contents($request_url);
+
+	            $send_count = 1;
+	            unset($_POST['callback']);
+	        }
+
+	    ?>
+
+	    						<form action="" method="POST">
+									<div>
+										<div class="row">
+											<div class="col-6 col-12-medium">
+												Спасибо за ваш отклик! В ближайщее время сотрудники Вам перезвонят! Ждите звонка!
+											</div>
+											<div class="col-12">
+												<input type="submit" name="callbackagain" value="Отправить еще раз" />
+											</div>
+										</div>
+									</div>
+								</form>
+    	<?php endif; ?>	
+							</article>
+		
+
+
+					</div>
+
+				<!-- Footer -->
+					<div id="footer">
+						<ul class="copyright">
+							<li>&copy; Rovshanchik.</li><li>ALL RIGHTS RESERVED. 2018 | POWERED BY: <a href="http://www.dataproger.gq/">DATAPROGER</a></li>
+						</ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script>
+	            $(document).ready(function(){ 
+	            $('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();
+	            }); 
+        	</script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
